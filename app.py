d80db22f4029b4e409702bcb1f1b09466c97997b
fvaneynde <frederik.van-eynde@dxc.com>
2021-07-12 12:00:09 +0200

    .

--- app.py
+++ app.py
@@ -1,19 +1,52 @@
-from flask import Flask
-
-
+# app.py
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+# http://localhost:5000/getmsg/?name=xxx
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    name = request.form.get('name')
+    print(name)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if name:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
 @app.route('/')
 def index():
-    return "hello root"
-
-@app.route('/world')
-def world():
-    return "hello world"
-
-@app.route('/town')
-def town():
-    return "hello town"
+    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+# Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
